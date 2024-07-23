906bd4bcada8f989c6336e71d1e3daee7d783824
bevane <bevane@live.com>
2024-07-23 08:55:58 +0700

    build: add css loading and image loading to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,23 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Restaurant Page',
       template: 'src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
