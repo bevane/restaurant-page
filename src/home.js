d6d168c27e6537901dd4d5f90aff59f9561d7a37
bevane <bevane@live.com>
2024-07-24 08:29:18 +0700

    feat: add image credit

--- src/home.js
+++ src/home.js
@@ -9,11 +9,16 @@ export default function homePage() {
   const coverImage = document.createElement("img");
   coverImage.src = Cover
 
+  const imgCredit = document.createElement("div");
+  imgCredit.innerHTML = 'Photo by <a href="https://unsplash.com/@annapelzer?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Anna Pelzer</a> on <a href="https://unsplash.com/photos/bowl-of-vegetable-salads-IGfIGP5ONV0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>';
+
+
   const intro = document.createElement("p");
   intro.innerText = "Enjoy a variety of tasty food that is also healthy! Our selection is based around the mediterranean cuisine with some interesting changes";
 
   content.appendChild(headline);
   content.appendChild(coverImage);
+  content.appendChild(imgCredit);
   content.appendChild(intro);
 }
 
